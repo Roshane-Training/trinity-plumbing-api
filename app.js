f0d4ea17bd50d71442045b482494ca8bff5523d3
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-09-14 19:32:57 -0500

    created admin crud

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const MONGODB_URI = PRODUCTION ? process.env.MONGODB_URI_PROD : process.env.MONG
 const FRONTEND_URL = PRODUCTION ? process.env.FRONTEND_URL_PROD : process.env.FRONTEND_URL
 
 const indexRouter = require('./routes/index.routes')
-const userRouter = require('./routes/users.routes')
 const authRouter = require('./routes/auth.routes')
+const userRouter = require('./routes/users.routes')
+const adminRouter = require('./routes/admin.routes')
 const productRouter = require('./routes/products.route')
 const serviceRouter = require('./routes/services.route')
 const plumberRouter = require('./routes/plumbers.route')
@@ -29,6 +30,7 @@ const API_VERSION = '/api/v1'
 app.use(API_VERSION + '/', indexRouter)
 app.use(API_VERSION + '/auth', authRouter)
 app.use(API_VERSION + '/users', userRouter)
+app.use(API_VERSION + '/admins', adminRouter)
 app.use(API_VERSION + '/products', productRouter)
 app.use(API_VERSION + '/services', serviceRouter)
 app.use(API_VERSION + '/plumbers', plumberRouter)
@@ -41,8 +43,8 @@ mongoose
 			console.log(
 				`\r====================================================================\n
 				\r\t[*] Endpoints for \x1b[34m${NAME}\x1b[0m are available [*]\n
-				\r\t[*] Local: \x1b[4m\x1b[32mhttp://localhost:${PORT}/api/v1\x1b[0m\r
-				\r\t[*] Your Network: \x1b[4m\x1b[32m${`http://${ip.address()}`}:${PORT}/api/v1\x1b[0m\r
+				\r\t[*] Local: \x1b[4m\x1b[32mhttp://localhost:${PORT}${API_VERSION}\x1b[0m\r
+				\r\t[*] Your Network: \x1b[4m\x1b[32m${`http://${ip.address()}`}:${PORT}${API_VERSION}\x1b[0m\r
 				\r\n====================================================================`
 			)
 		})
